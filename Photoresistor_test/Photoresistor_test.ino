e036a6d38405c3fac05a296f649de23cda22578a
Frostmanfunk <frostmanfunk@gmail.com>
2022-01-25 16:25:51 +0000

    bæta við LED og Photoresistor test.

---
+++ Photoresistor_test/Photoresistor_test.ino
@@ -0,0 +1,35 @@
+const int pResistorRight = A1;
+const int pResistorLeft = A2;
+const int ledPin = 9;
+
+// Ljósviðnám gildi
+int valueRight;
+int valueLeft;
+int valueLight;
+
+void setup() {
+  Serial.begin( 9600 );
+  pinMode(ledPin, OUTPUT);
+  pinMode(pResistorRight, INPUT);
+  pinMode(pResistorLeft, INPUT);
+  digitalWrite(ledPin, HIGH);
+}
+
+void loop() {
+  valueRight = analogRead(pResistorRight);
+  valueLeft = analogRead(pResistorLeft);
+
+  valueLight = 500;     // Þurfum að fínstilla þetta fyrir keppni
+
+  /*
+  if (valueLight > 500 ) {
+    Serial.print("Frosti segir hi");
+  }
+  else {
+    Serial.print("Huldar");
+  }
+  */
+  Serial.println("Hægri: " + String(valueRight) + "   Vinstri: " + String(valueLeft));
+  
+  delay(1000);    // 1 second delay
+}
